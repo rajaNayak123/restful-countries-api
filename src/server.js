73b58e7e8444cb4ba4fa909a022e815384ee266a
rajaNayak123 <nayakraja151@gmail.com>
2025-11-29 16:45:04 +0530

    import the middleware and router

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import http from 'http';
+import { logger } from './middleware/logger.js';
+import { router } from './routes/router.js';
 
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer((req, res) => {
-
+const server = http.createServer(async (req, res) => {
+  // 1. Logging Middleware
+  logger(req, res);
+  
+  // 2. Run Router
+  await router(req, res);
 });
 
 server.listen(PORT, () => {
